e721cf1a53f1fc50d83bd9c54f3b737443b0635d
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-06 13:21:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   patch 'posts/:id' do
     @post = Post.find(params[:id])
     @post.update(params)
-    redirect 'posts/params[:id]'
+    redirect 'posts/#{params[:id]}'
   end
 
 end
